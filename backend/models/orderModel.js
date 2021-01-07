add23ff7f58b59f6780e4df1c887be2946008b00
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-01-07 02:27:58 -0500

    Shopping Process screen done the visual

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -33,7 +33,7 @@ const orderSchema = new mongoose.Schema(
       update_time: { type: String },
       email_address: { type: String },
     },
-    taxePrice: { type: Number, required: true, default: 0.0 },
+    taxPrice: { type: Number, required: true, default: 0.0 },
     shippingPrice: { type: Number, required: true, default: 0.0 },
     totalPrice: { type: Number, required: true, default: 0.0 },
     isPaid: { type: Boolean, required: true, default: false },
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,32 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { saveShippingAddress } from '../actions/cartActions'
-import FormContainer from '../components/FormContainer'
+import { createOrder } from '../actions/orderActions'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
-import { Link } from 'react-router-dom'
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
   const dispatch = useDispatch()
 
   const cart = useSelector((state) => state.cart)
 
   //Calculate prices
-  cart.itemsPrice = cart.cartItems.reduce(
-    (acc, item) => acc + item.price * item.qty,0
-  ).toFixed(2)
+  cart.itemsPrice = cart.cartItems
+    .reduce((acc, item) => acc + item.price * item.qty, 0)
+    .toFixed(2)
   //shipping
   cart.shippingPrice = (cart.itemsPrice > 100 ? 0 : 100).toFixed(2)
-  cart.taxPrice = Number((0.15*cart.itemsPrice).toFixed(2)).toFixed(2)
-cart.totalPrice = Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)
-  /**round on 2 digit */
-  const addDecimals = (num)=>{
-      return (Math.round(num * 100)/100).toFixed(2)
-  }
+  cart.taxPrice = Number((0.15 * cart.itemsPrice).toFixed(2)).toFixed(2)
+  cart.totalPrice =
+    Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)
 
-  const placeOrderHandler = () => {
-    console.log('order')
+  /**round on 2 digit */
+  // const addDecimals = (num) => {
+  //   return (Math.round(num * 100) / 100).toFixed(2)
+  // }
+  const orderCreate = useSelector((state) => state.orderCreate)
+  const { order, success, error } = orderCreate
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`)
+    }
+
+    //eslint-disable-next-line
+  }, [history, success])
+
+  function placeOrderHandler() {
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    )
   }
 
   return (
@@ -117,6 +138,9 @@ cart.totalPrice = Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(
                   <Col>${cart.totalPrice}</Col>
                   <Col></Col>
                 </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
               </ListGroup.Item>
               <ListGroup.Item>
                 <Button
